22975c817dbed3b1cf7fa3083d7e0c8b1c25f46f
YoRu Tech <tech.niko.coding@gmail.com>
2024-02-22 09:03:42 +0500

    Version 1

---
+++ calculator.js
@@ -0,0 +1,22 @@
+function appendToDisplay(input)
+{
+  display.value += input;
+}
+
+const display = document.getElementById("display");
+
+function clearDisplay()
+{
+  display.value = "";
+}
+function calculate()
+{
+  try
+  {
+    display.value = eval(display.value);
+  }
+  catch(error)
+  {
+    display.value = "Error";
+  }
+}
